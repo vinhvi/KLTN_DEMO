7ea0b74a5e117cd8930406775b6c7d188c6ef5dc
vinhvi <vithevinh123@gmail.com>
2023-02-20 10:38:06 +0700

    start after vaction long time

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
-import mysql from "mysql";
+import mysql from "mysql2";
 
 export const db = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "666666",
+  password: "vinh123",
   database: "demo_kltn",
 });
 
